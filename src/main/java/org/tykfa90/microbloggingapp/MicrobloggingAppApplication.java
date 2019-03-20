e8e1456c3c15cf0505aacf1f95dac90c46243ec7
Konstanty Białek <tykfa90@gmail.com>
2019-03-20 14:51:28 +0100

    Initial commit

---
+++ src/main/java/org/tykfa90/microbloggingapp/MicrobloggingAppApplication.java
@@ -0,0 +1,13 @@
+package org.tykfa90.microbloggingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobloggingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobloggingAppApplication.class, args);
+	}
+
+}
